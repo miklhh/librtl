4a080486c1080aacb4b1f581144a4420f0bcebf7
Mikael Henriksson <mikael.henriksson@liu.se>
2025-09-26 14:32:59 +0200

    build: add Catch2

--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,28 @@
 #include <absl/container/flat_hash_map.h>
+
 #include <fmt/format.h>
+
+#include <catch2/catch_test_macros.hpp>
+
 
 #include <iostream>
 #include <string>
 
 
-int main()
+namespace rtl {
+    void haha() {
+        std::cout << "HAHA" << std::endl;
+    }
+}
+
+
+
+TEST_CASE("flat_hash_map")
 {
     absl::flat_hash_map<int, std::string> map{ 
         { 1, "abc123" },
         { 2, "hello" },
         { 1337, "elite" },
     };
-    for (auto [key, val] : map) {
-        std::cout << fmt::format("key: {}, val: {}", key, val) << std::endl;
-    }
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+    REQUIRE(map[1] == "elite");
 }
